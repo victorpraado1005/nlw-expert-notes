d7ccc198f99df7dad92f8ae7ec63c4e8e8c8756a
Victor Prado <prado.victor1005@gmail.com>
2024-02-06 16:52:38 -0300

    feat: estilizando os toast da aplicacao.

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         unstyled: true,
         classNames: {
           toast: 'w-full p-4 flex items-center gap-2 rounded-md text-lime-400',
-          closeButton: 'bg-lime-400',
           error: 'bg-red-400 text-slate-950',
           success: 'bg-slate-800',
         }
