2632fe0cf8f3ba6f80a2c43be04f1e5a3f6af2a2
Victor Prado <prado.victor1005@gmail.com>
2024-02-06 16:49:12 -0300

    feat: estilizando os toast da aplicacao.

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,16 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <Toaster richColors />
+    <Toaster
+      toastOptions={{
+        unstyled: true,
+        classNames: {
+          toast: 'w-full p-4 flex items-center gap-2 rounded-md text-lime-400',
+          closeButton: 'bg-lime-400',
+          error: 'bg-red-400 text-slate-950',
+          success: 'bg-slate-800',
+        }
+      }}
+    />
   </React.StrictMode>,
 )
